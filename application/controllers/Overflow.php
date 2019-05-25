1243662990e5f03cc1c34691b659799f3ab7337e
supakjack <48548611+supakjack@users.noreply.github.com>
2019-05-25 12:43:30 +0700

    Update Overflow.php

--- application/controllers/Overflow.php
+++ application/controllers/Overflow.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-include_once('UniDev.php');
+include_once(dirname(__FILE__)."/UniDev.php");
 class Overflow extends UniDev {
 	public function __construct() {
         parent::__construct(); 
